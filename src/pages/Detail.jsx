75c7d4fc0949617968c46c7b19921797a987ff08
moduri <dm029@naver.com>
2022-08-21 01:09:13 +0900

    7commit

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -39,22 +39,110 @@ function Detail() {
             {state?.img?.length ? (
               <>
                 <SmallPictureOne src={`${state?.img[1]}`} />
-                <SmallPictureTwo src={`${state?.img[2]}`} />
-                <SmallPictureThree src={`${state?.img[3]}`} />
-                <SmallPictrueFour src={`${state?.img[4]}`} />
               </>
+            ) : null}
+            {state?.img?.length ? (
+              <SmallPictureTwo src={`${state?.img[2]}`} />
+            ) : null}
+            {state?.img?.length ? (
+              <SmallPictureThree src={`${state?.img[3]}`} />
+            ) : null}
+            {state?.img?.length ? (
+              <SmallPictrueFour src={`${state?.img[4]}`} />
             ) : null}
           </PictureSmallBox>
         </AllPictures>
       </TopMainBox>
       <MiddleMainBox>
         <MiddleLeftBox>
-          {state?.auth}
-          {state?.category}
-          {state?.content}
+          <DescribeTitle>
+            {state?.auth}님이 호스팅하는 {state?.category}
+          </DescribeTitle>
+          <div>최대 인원 2명 ・ 침실 1개 ・ 침대 1개 ・ 욕실 1개</div>
+          <hr />
+          <div style={{ display: "flex" }}>
+            <img
+              src="/images/Medal.png"
+              alt="medal"
+              style={{ height: "100%", marginRight: "10px" }}
+            />
+            <div>
+              <div>{state?.auth}님은 슈퍼호스트입니다</div>
+              <div>
+                슈퍼호스트는 풍부한 경험과 높은 평점을 자랑하며 게스트가
+                숙소에서 편안히 머무를 수 있도록 최선을 다하는 호스트입니다.
+              </div>
+            </div>
+          </div>
+          <div style={{ display: "flex", marginTop: "20px" }}>
+            <img
+              src="/images/Map.png"
+              alt="map"
+              style={{ height: "100%", marginRight: "10px" }}
+            />
+            <div>
+              <div>훌륭한 숙소 위치</div>
+              <div>
+                최근 숙박한 게스트 중 90%가 위치에 별점 5점을 준 숙소입니다.
+              </div>
+            </div>
+          </div>
+          <div style={{ display: "flex", marginTop: "20px" }}>
+            <img src="/images/CalendarMini.png" alt="CalMini" />
+            <div style={{ marginTop: "8px", marginLeft: "10px" }}>
+              8월 26일 전까지 무료로 취소하실 수 있습니다.
+            </div>
+          </div>
+          <hr />
+          <div>
+            <div>
+              <span>일부 정보는 원어로 표시됩니다.</span>
+              <span
+                onClick={() => {
+                  alert("아직 미구현 입니다");
+                }}
+                style={{
+                  fontWeight: "600",
+                  textDecoration: "underline",
+                  marginLeft: "10px",
+                  cursor: "pointer",
+                }}
+              >
+                번역
+              </span>
+            </div>
+            <div
+              style={{
+                marginTop: "10px",
+              }}
+            >
+              {state?.content}
+            </div>
+          </div>
+          <hr />
+          <div
+            style={{
+              marginBottom: "10px",
+            }}
+          >
+            숙박장소
+          </div>
+          {state?.img?.length ? (
+            <SmallPictrueFour src={`${state?.img[1]}`} />
+          ) : null}
+          <hr />
+          <img src="/images/Convenient.png" />
+          <MoreConvenient
+            onClick={() => {
+              alert("아직 미구현 입니다");
+            }}
+          >
+            편의시설 모두 보기
+          </MoreConvenient>
+          <img src="/images/Calendar.png" alt="Calender" />
         </MiddleLeftBox>
         <MiddleRightBox>
-          <div>{state?.price}</div>
+          <StickyTopBox>{state?.price}</StickyTopBox>
         </MiddleRightBox>
       </MiddleMainBox>
       <Comment>댓글창</Comment>
@@ -103,6 +191,8 @@ const TitleBottonRight = styled.div`
   }
 `;
 
+// ㅡㅡㅡㅡㅡ사진ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ사진ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ사진ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
 const AllPictures = styled.div`
   display: flex;
   margin-top: 20px;
@@ -130,7 +220,6 @@ const PictureSmallBox = styled.div`
 
 const SmallPictureOne = styled.img`
   margin: auto;
-  background-color: #8d7e62;
 
   width: 48%;
   height: 46%;
@@ -139,7 +228,6 @@ const SmallPictureOne = styled.img`
 
 const SmallPictureTwo = styled.img`
   margin: auto;
-  background-color: #8d7e62;
 
   width: 48%;
   height: 46%;
@@ -149,7 +237,7 @@ const SmallPictureTwo = styled.img`
 
 const SmallPictureThree = styled.img`
   margin: auto;
-  background-color: #8d7e62;
+
   width: 48%;
   height: 46%;
   margin-top: 2px;
@@ -157,12 +245,14 @@ const SmallPictureThree = styled.img`
 
 const SmallPictrueFour = styled.img`
   margin: auto;
-  background-color: #8d7e62;
+
   width: 48%;
   height: 46%;
   margin-top: 2px;
   border-bottom-right-radius: 10px;
 `;
+
+// ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ중간 설명부분 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 
 const MiddleMainBox = styled.div`
   display: flex;
@@ -172,26 +262,41 @@ const MiddleMainBox = styled.div`
 
 const MiddleLeftBox = styled.div`
   width: 63%;
-  background-color: gray;
-  height: 1000px;
+  /* background-color: #e4dada; */
+  height: 100%;
 `;
 
 const MiddleRightBox = styled.div`
   width: 35%;
   margin-left: 2%;
-  background-color: #c29c9c;
-  height: 1000px;
-  div {
-    position: sticky;
-    background-color: wheat;
-    height: 100px;
-    padding-top: 30px;
-    top: 0;
-  }
+  height: 1500px;
+`;
+
+const DescribeTitle = styled.div`
+  font-weight: 500;
+  font-size: 24px;
+`;
+
+const MoreConvenient = styled.button`
+  padding: 10px;
+  margin-left: 380px;
+  border: 1px solid black;
+  background-color: white;
+  border-radius: 9px;
+  margin-bottom: 30px;
+`;
+
+// ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ중간 설명 오른쪽부분ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+const StickyTopBox = styled.div`
+  position: sticky;
+  background-color: #c7c998;
+  height: 300px;
+  padding-top: 30px;
+  top: 30px;
 `;
 
 const Comment = styled.div`
-  background-color: black;
+  background-color: #6bbf84;
   width: 100%;
   height: 500px;
 `;
