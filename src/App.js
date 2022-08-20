28849effbcfe71ba8e3fc6f89b4b32f46e4848e4
YuncheolKim <gakphe@gmail.com>
2022-08-20 16:47:09 +0900

    뷰 중간작업

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
-
+import CommentsLayout from "./components/commentsLayout";
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
-  return <div className='App'></div>;
+  return <div className='App'>
+    <CommentsLayout/>
+  </div>;
 }
 
 export default App;
