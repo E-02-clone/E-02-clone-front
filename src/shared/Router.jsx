732a99623ffed78d683d3e9f58b2a15a737afb9c
moduri <dm029@naver.com>
2022-08-24 16:19:02 +0900

    newstart2

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,24 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
-import Detail from '../pages/Detail'
-import Test from '../pages/Test';
-import WishLists from '../pages/WishLists';
-import Wish from '../pages/Wish';
-
+import Detail from "../pages/Detail";
+import Test from "../pages/Test";
+import WishLists from "../pages/WishLists";
+import Wish from "../pages/Wish";
+import WriteTwo from "../pages/WriteTwo";
 
 function Router() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/detail/:id" element={<Detail />} />
-                <Route path="/test" element={<Test />} />
-                <Route path="/wishlists" element={<WishLists />} />
-                <Route path="/wishlists/:id" element={<Wish />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/test" element={<Test />} />
+        <Route path="/wishlists" element={<WishLists />} />
+        <Route path="/wishlists/:id" element={<Wish />} />
+        <Route path="/write/:id" element={<WriteTwo />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default Router;
-
