7924ce5fbe54aa71baffc874a2cbddbb27fc4c92
Jongco <narira0922@gmail.com>
2022-08-24 16:07:36 +0900

    ㄹㅇㄴ

--- src/components/commentsLayout.jsx
+++ src/components/commentsLayout.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import './css/commentsLayout.css'
 import Comments from "./comments";
 import SeeAllComments from "./seeAllComments";
@@ -7,29 +7,29 @@ import WriteComments from "./writeComments";
 
 
 const CommentsLayout = () => {
-    
+
     const [modalOpen, setModalOpen] = useState(false);
     const [write, setWrite] = useState(false);
 
     const onClickWrite = () => {
-        setWrite((status)=>!status);
+        setWrite((status) => !status);
     }
 
     const onClickModal = () => {
-        setModalOpen((status)=>!status);
+        setModalOpen((status) => !status);
     };
 
     return (
         <div className="commentsLayout">
-        <div className="commentsSubject">
-            <p style={{fontSize:'30px'}}>총점</p> <button onClick={onClickWrite}>후기 작성</button>
-            {write ? <WriteComments/> :  <WriteComments/>}
-            <p>점수</p>
-        </div>
-            <Comments onClick={onClickModal}/>
+            <div className="commentsSubject">
+                <p style={{ fontSize: '30px' }}>총점</p> <button onClick={onClickWrite}>후기 작성</button>
+                {write ? <WriteComments /> : <WriteComments />}
+                <p>점수</p>
+            </div>
+            <Comments onClick={onClickModal} />
             <button className="modalButton" onClick={onClickModal}>모든 후기 보기</button>
             <div className={modalOpen ? "modalOpen" : "modalClose"}>
-            {modalOpen ? <SeeAllComments onClick={onClickModal}/> : <SeeAllComments/>}
+                {modalOpen ? <SeeAllComments onClick={onClickModal} /> : <SeeAllComments />}
             </div>
         </div>
     )
