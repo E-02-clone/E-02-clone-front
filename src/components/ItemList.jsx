4e386d577474cab7772789b6a89db0becf187c29
Jong1co <95389265+Jong1co@users.noreply.github.com>
2022-08-25 19:44:30 +0900

    Update ItemList.jsx

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,17 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
-import { getMainItems } from '../app/slice/mainSlice';
+import { getMainItems, getSubItems } from '../app/slice/mainSlice';
 import ItemElement from './ItemElement';
 
 const ItemList = ({ select, setModal, modal }) => {
-    const items = useSelector(state => state.main.data?.data)
+
+    const items = useSelector(state => state.main.data?.data);
     console.log(items)
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
+    const [page, setPage] = useState(1);
+    console.log(page)
+
+    const handleScroll = () => {
+        const scrollHeight = document.documentElement.scrollHeight;
+        const scrollTop = document.documentElement.scrollTop;
+        const clientHeight = document.documentElement.clientHeight;
+
+        console.log('스크롤 이벤트 발생');
+
+        if (scrollTop + clientHeight >= scrollHeight) {
+            console.log('페이지 끝에 스크롤이 닿았음');
+            setPage((prev) => prev + 1);
+        }
+    };
 
     useEffect(() => {
-        dispatch(getMainItems())
-    }, [dispatch])
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    useEffect(() => {
+        dispatch(getSubItems(page))
+    }, [page])
 
     if (select === '전체보기') {
         return (
